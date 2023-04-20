43fe81cee0bb770ba6240cdaace85db9a666546b
Lukasz Tlalka <lukasz.tlalka@netblink.net>
2023-04-20 11:20:06 +0200

    Removed dd on the github actions test

--- src/tests/github-runner.sh
+++ src/tests/github-runner.sh
@@ -19,8 +19,8 @@ mkdir           /tmp/mnt-limited_dir_5M/lvl1/lvl2
 mkdir           "/tmp/mnt-limited_dir_5M/lvl1/lvl2/lvl3 with spaces "
 chmod 422       "/tmp/mnt-limited_dir_5M/lvl1/lvl2/lvl3 with spaces "
 
-dd if=/dev/zero of="/tmp/mnt-limited_dir_5M/lvl1/lvl2/lvl3 with spaces /file1 with spaces .txt" bs=2M count=1
-dd if=/dev/zero of=/tmp/mnt-limited_dir_5M/lvl1/lvl2/file2.txt bs=2M count=1
+echo "data" > "/tmp/mnt-limited_dir_5M/lvl1/lvl2/lvl3 with spaces /file1 with spaces .txt"
+echo "more data" > /tmp/mnt-limited_dir_5M/lvl1/lvl2/file2.txt
 
 touch "/tmp/mnt-limited_dir_5M/lvl1/empty file1.txt"
 touch /tmp/mnt-limited_dir_5M/lvl1/empty-file2.txt
